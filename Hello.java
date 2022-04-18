3b1e4facac579210f9d2f2b93ce9651ea613594f
刘浩 <69785949+liuhao5201314@users.noreply.github.com>
2022-04-18 21:15:46 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+//这是java的一个hello程序,演示java开发
+//1.public class Hello 表示Hello是一个类，是一个public（公有）的类
+//2.Hello{} 表示一个类的开始和结束
+//3.public static void main(string[] args) 表示一个主方法，即我们程序的入口
+//4.main（）{}表示方法的开始和结束
+//5.System.out.println("hello,world~");//表示输出Hello word~到屏幕/分号表示语句结束;
+public class Hello{
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println("刘浩开始学习java");
+	}
+}
