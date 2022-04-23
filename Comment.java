d2e7a0a5188ecd3fc4e3978207d96b325cbe61b1
刘浩 <69785949+liuhao5201314@users.noreply.github.com>
2022-04-23 20:16:59 +0800

    3.java注释学习

---
+++ Comment.java
@@ -0,0 +1,34 @@
+//演示注释使用
+//文档注释
+//G:\兴趣\java\text4> -g g:\\G:\兴趣\java\text4\javatemp -author -version Comment.java
+/**
+ * @author  刘浩
+ * @version  1.0
+ */
+public class Comment{
+	public static void main(String[] args){
+		//单行注释
+		/*多行注释（不允许嵌套使用）*/
+	
+	}
+}
+//java简单注意事项
+//1.类方法的注释，要以javadoc的方式来写
+//2.非javadoc的注释一般是给维护者来看的，告诉作者怎么书写，为什么这么写，注意问题等
+//3.tab进行缩进右移  shift tab左移
+//4.运算符和=二边习惯加空格，方便读
+//5.源文件要使用UTF-8进行保存
+//6.行宽不超过80字符
+//7.代码编写次行风格和行尾风格
+//
+//
+//DOS基本原理
+//G:\兴趣\java\text4>md g:\\兴趣\java\text4\hsptemp
+//md创建一个文件夹
+//G:\兴趣\java\text4>rd g:\\兴趣\java\text4\hsptemp
+//rd删除一个文件夹
+//Dos系统1.接收指令2.解析指令3.执行指令--->作用于windows目录
+//
+//
+//相对路径和绝对路径
+//
