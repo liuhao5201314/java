2992d6d97ef1883532ff8618efe4bfbaa881d835
刘浩 <69785949+liuhao5201314@users.noreply.github.com>
2022-04-24 21:31:12 +0800

    7.java加号的学习

---
+++ Jiahao.java
@@ -0,0 +1,18 @@
+//加号
+//二边为数值型，则做加法运算
+//一边为字符则做拼接运算
+//运算顺序从左向右
+public class Jiahao{
+	public static void main(String[] args){
+		System.out.println(100+98);//198
+		System.out.println("100"+98);//10098
+		System.out.println(100+3+"hello");//103hello
+		System.out.println("hello"+100+3);//hello100+3=hello1003
+	}
+}
+
+class Jiahao2{
+	public static void main(String[] args){
+		
+	}
+}
